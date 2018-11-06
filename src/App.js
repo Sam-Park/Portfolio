2aa65e353e99a9f485c0f83d4433955286204ec7
unknown <sam.park.email@gmail.com>
2018-11-05 20:42:41 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,88 +1,120 @@
-import React, { Component } from 'react';
-import './App.css';
-import Falcano from './images/falcanoLanding.PNG';
-import { TweenMax, Bounce, Elastic, Power4 } from 'gsap';
-import TimelineMax from 'gsap';
-import Tilt from 'react-tilt';
-import ReactTilt  from 'react-universal-tilt';
-  function myFunc(el) {
-    el.style.backgroundColor = '#000000';
-  }
+import React, { Component } from "react";
+import "./App.css";
+import Falcano from "./images/falcanoLanding.PNG";
+import Github from "./images/GitHub-Mark-Light-32px.png"
+import { TweenMax, Power4 } from "gsap";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import ReactTilt from "react-universal-tilt";
+function myFunc(el) {
+  el.style.backgroundColor = "#000000";
+}
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.myElement = null;
     this.myTween = null;
     this.myStagger = null;
-    // this.animation = new TimelineMax();
   }
-  
-
 
   componentDidMount() {
-    let staggerObjs = [".nav-h", ".nav-h1", ".nav-h2", ".nav-h3"]
-    // this.myTween = TweenMax.to(this.myElement, 1, { y: 45,});
-    this.myStagger = TweenMax.staggerFrom(staggerObjs, 1, {opacity:0, x:180, ease:Power4.easeOut}, 0.40);
-    // this.animation.to('.nav-h', 1, {y:'150'})
+    let staggerObjs = [".nav-h", ".nav-h1", ".nav-h2", ".nav-h3", ".nav-h4"];
+    this.myStagger = TweenMax.staggerFrom(
+      staggerObjs,
+      1,
+      { opacity: 0, x: 180, ease: Power4.easeOut },
+      0.4
+    );
   }
   render() {
     const style = {
-      'border': '1px solid #333',
-      width: '350px',
-      height: '350px',
-      marginLeft: '40%',
-      display: 'flex',
-      alignContent: 'center',
-      justifyContent: 'center',
+      border: "1px solid #333",
+      width: "450px",
+      height: "400px",
+      display: "flex",
+      alignContent: "center",
+      justifyContent: "center"
     };
     return (
       <div className="App">
-      <div className="Landing-Container">
-      <div className="Left-container">
-
-          <p className="Name-slide" ref={p => this.myElement = p}>
-            Samuel
-          </p>
-          <p className="Sur-Name-Slide">
-            Park
+        <div className="Landing-Container">
+          <div className="Nav-container">
+            <p className="Name-slide" ref={p => (this.myElement = p)}>
+              Samuel
             </p>
-          <h4 className="nav-h">Projects</h4>
-          <h4 className="nav-h1">Resume</h4>
-          <h4 className="nav-h2">Stuff</h4>
-          <h4 className="nav-h3">More Stuff</h4>
-      </div>
-      <div className="Center-Container-About">
-      <h1 className="About-Me">about <span className="About-Span">me.</span></h1>
-      </div>
-      </div>
+            <p className="Sur-Name-Slide">Park</p>
+            <AnchorLink className="nav-h" href="#projects">
+              Projects
+            </AnchorLink>
+            <AnchorLink className="nav-h1">Resume</AnchorLink>
+            <AnchorLink className="nav-h2">Skills</AnchorLink>
+            <AnchorLink className="nav-h3" href="#aboutme">
+              About Me
+            </AnchorLink>
+            <AnchorLink className="nav-h4" href="#contact">
+            Contact
+            </AnchorLink>
+          </div>
+          <div id="aboutme" className="Center-Container-About">
+            <h1 className="About-Me">
+              about <span className="About-Span">me.</span>
+            </h1>
+          </div>
+        </div>
 
-    <div className="Center-Container-Projects">
-    <div className="Falcano-Wrapper">
-      <ReactTilt
-        options={{
-          speed: 1000,
-          scale: 1.02,
-          'shine-opacity': .3,
-          shadow: 'true',
-          shine: 'true',
-          max: 5,
-          perspective: 500,
-          onMouseMove: el => myFunc(el)
-        }}
-        style={style}
-        
-        className="tilt-elem my-tilt"
-        
-        tiltChange={this.output}
-        >
-      <div className="Falcano-Card">
-        <img 
-        className="Falcano-Image"
-        src={Falcano} />
+        <div id="projects" className="Center-Container-Projects">
+          <h1 className="Projects-H1">
+            Proje
+            <span className="Projects-H1-Span">cts.</span>
+          </h1>
+
+          <h1 className="Falcano-H1">Fal<span className="Falcano-H1-Span">cano.</span></h1>
+          <div className="Falcano-Wrapper">
+          <div className="Falcano-Card-Wrapper">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.falcano.app">
+            <ReactTilt
+              options={{
+                speed: 1000,
+                scale: 1.01,
+                "shine-opacity": 0.3,
+                shadow: "true",
+                shine: "true",
+                max: 5,
+                perspective: 500,
+                onMouseMove: el => myFunc(el)
+              }}
+              style={style}
+              className="React-Tilt-Card"
+              tiltChange={this.output}
+              >
+
+              <div className="Falcano-Card">
+                <img
+                  alt="Project Thumbnail"
+                  className="Falcano-Image"
+                  src={Falcano}
+                  />
+              </div>
+            </ReactTilt>
+          </a>
+        <div className="Project-Links">
+        <a className="GitHub-Logo" target="_blank" rel="noopener noreferrer" href="https://github.com/Lambda-School-Labs/CS10-flight-log">
+          <img  alt="GitHub logo" src={Github} ></img>
+        </a>
+            <a className="GitHub-Link" target="_blank" rel="noopener noreferrer" href="https://github.com/Lambda-School-Labs/CS10-flight-log">Github Link</a>
+          </div>
         </div>
-        </ReactTilt>
-        </div>
+            <p className="Falcano-Paragraph">
+              Falcano is my Capstone project for Lambda School, it is a flight log
+              for enthusiast aviators and pilots in training. Falcano records and aggregates your
+              totals for many different fields for specific aircraft and license types.
+               I worked in a
+              team of four with a project manager over the course of one month.
+              Using <span className="Projects-Paragraph-Span">Django</span> and <span className="Projects-Paragraph-Span">React</span> for our main tech stack, teaching ourselves
+              how to use django along the way.
+            </p>
+          </div>
+          
         </div>
       </div>
     );
